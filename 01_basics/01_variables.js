4bc41397a2a97eb11ea1f1b0f7e7182891d74619
snv1803 <165540617+snv1803@users.noreply.github.com>
2024-08-03 07:02:14 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345
+let accountEmail = "xyz@gmail.com"
+var accountPassword = "Ayo"
+accountCity = "Surat"
+let accountState;
+
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
