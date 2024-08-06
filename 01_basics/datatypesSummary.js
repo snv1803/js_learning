503d7a4acbc6abad9fd94af51605816e85d65a40
snv1803 <165540617+snv1803@users.noreply.github.com>
2024-08-06 06:45:17 +0000

    Datatype Summary

--- 01_basics/datatypesSummary.js
+++ 01_basics/datatypesSummary.js
@@ -2,18 +2,18 @@
 
 // There are 7 categories : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.7
+// const score = 100
+// const scoreValue = 100.7
 
-const  isLoggedIn = false
-const outsideTemp = null
+// const  isLoggedIn = false
+// const outsideTemp = null
 
-let userEmail;
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id == anotherId);
+// console.log(id == anotherId);
 
 // const bigNumber = 1234567890n //n represents bigInt
 
@@ -22,14 +22,38 @@ console.log(id == anotherId);
 
 // Array, Object, Function
 
-const marvel = ["Ironman", "Captain America", "Black Widow", "Hawkeye", "Hulk", "Thor"]
-let myObj = {
-    name : "Saniya",
-    age : 20,
+// const marvel = ["Ironman", "Captain America", "Black Widow", "Hawkeye", "Hulk", "Thor"]
+// let myObj = {
+//     name : "Saniya",
+//     age : 20,
+// }
+
+// const myFunction = function(){
+//     console.log("Saniya");
+//     }
+
+// console.log(typeof myFunction);
+
+
+// *******************
+// Stack (Primitive), Heap (Non-Primitive)
+
+// let myName = "Saniya"
+
+// let anotherName = myName
+// anotherName = "Lokid"
+
+// console.log(anotherName);
+// console.log(myName);
+
+let userOne = {
+    email : "xyz@gmail.com",
+    upi : "user@ybl"
 }
 
-const myFunction = function(){
-    console.log("Saniya");
-    }
+let userTwo = userOne
 
-console.log(typeof myFunction);
+userTwo.email = "abc@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
