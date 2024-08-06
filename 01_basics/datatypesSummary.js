f3b4e842cbc15ece1b689c28febe49751cc7adb0
snv1803 <165540617+snv1803@users.noreply.github.com>
2024-08-06 05:03:13 +0000

    Datatype summary

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,35 @@
+// ***Primitive Datatypes***
+
+// There are 7 categories : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.7
+
+const  isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+// const bigNumber = 1234567890n //n represents bigInt
+
+
+// ***Non-Primitive Datatype***
+
+// Array, Object, Function
+
+const marvel = ["Ironman", "Captain America", "Black Widow", "Hawkeye", "Hulk", "Thor"]
+let myObj = {
+    name : "Saniya",
+    age : 20,
+}
+
+const myFunction = function(){
+    console.log("Saniya");
+    }
+
+console.log(typeof myFunction);
