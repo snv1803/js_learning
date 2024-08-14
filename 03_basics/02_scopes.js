c71601042623d45b6c6365654b5e59a19c90ccb5
snv1803 <165540617+snv1803@users.noreply.github.com>
2024-08-14 06:43:01 +0000

    done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,6 +5,43 @@ if (true) {
     const b = 20
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one (){
+    const username = "Saniya"
+
+    function two () {
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    // two()   
+}
+
+// one()
+
+
+if (true) {
+    const username = "Saniya"
+    if (username === "Saniya") {
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);   
+}
+
+// console.log(username);
+
+console.log(addOne(5))
+
+function addOne (num) {
+    return num + 1
+}
+
+const addTwo = function(num) {
+    return num + 2
+}
+
+console.log(addTwo(5))
